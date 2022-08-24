69e84dfa7c35079f4cdc4a530b4002fddc0c8f2e
robertlestak <robert.lestak@umusic.com>
2022-08-23 22:53:13 -0700

    find path

--- pkg/pushx/pushx.go
+++ pkg/pushx/pushx.go
@@ -2,16 +2,40 @@ package pushx
 
 import (
 	"bytes"
+	"errors"
 	"io"
+	"os"
 	"os/exec"
 )
 
+func findExec(name string) (string, error) {
+	path, err := exec.LookPath(name)
+	if err != nil {
+		return "", err
+	}
+	if path == "" {
+		path = "/usr/local/bin/" + name
+	}
+	return path, nil
+}
+
 func Pushx(input io.Reader, args []string) error {
-	cmd := exec.Command("pushx", args...)
+	path, e := findExec("pushx")
+	if e != nil {
+		return e
+	}
+	cmd := exec.Command(path, args...)
+	cmd.Env = os.Environ()
 	cmd.Stdin = input
 	var out bytes.Buffer
 	var err bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &err
-	return cmd.Run()
+	if e != nil {
+		var err bytes.Buffer
+		err.WriteString(err.String())
+		err.WriteString(out.String())
+		return errors.New(err.String())
+	}
+	return nil
 }
